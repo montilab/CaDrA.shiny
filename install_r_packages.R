2e2ee96be13b95d8bdbf1cc2565e1e0629d6722d
RC-88 <lilychau999@gmail.com>
2023-12-11 12:17:54 -0500

    update install R packages

--- install_r_packages.R
+++ install_r_packages.R
@@ -1,28 +1,52 @@
 
-# Make sure BiocManager is installed
-if (!require('BiocManager', quietly = TRUE))
-  utils::install.packages('BiocManager')
+# Create a list of start-up packages 
+startup_packages <- c("BiocManager", "dplyr", "rvest", "xml2", "yaml")
 
-# Load BiocManager package
+# Select only the packages that aren't currently installed in the system
+install_startup_packages <- startup_packages[which(!startup_packages %in% utils::installed.packages())]
+
+# And finally we install the required packages
+for(pkg in install_startup_packages) BiocManager::install(pkg)
+
+# Load packages
 library(BiocManager)
+library(dplyr)
+library(rvest)
+library(xml2)
+library(yaml)
 
-# Required bioconductor packages
-bioconductor_pkgs <- c("CaDrA", "SummarizedExperiment", "GSVA", "GSEABase")
+# Get all available Bioconductor packages
+url <- 'https://www.bioconductor.org/packages/release/bioc/'
+biocPackages <- url %>% 
+  xml2::read_html() %>% 
+  rvest::html_table() %>%
+  .[[1]] %>%
+  dplyr::select(Package) %>% 
+  dplyr::mutate(source = 'BioConductor')
 
-# Select only the packages that aren't currently installed.
-install_bioconductor_lib <- bioconductor_pkgs[!bioconductor_pkgs %in% utils::installed.packages()]
+# Read in package dependencies in DESCRIPTION
+DESCRIPTION <- yaml::read_yaml("DESCRIPTION")
+
+# Extract all imports packages
+required_pkgs <- trimws(strsplit(DESCRIPTION$Imports, ",", fixed=TRUE)[[1]])
+
+# Extract all required bioconductor packages
+bioconductor_pkgs <- required_pkgs[which(required_pkgs %in% biocPackages$Package)]
+
+# Select only the packages that aren't currently installed in the system
+install_bioconductor_pkgs <- bioconductor_pkgs[which(!bioconductor_pkgs %in% utils::installed.packages())]
                         
-# And finally we install the missing bioconductor packages
-for(lib in install_bioconductor_lib) BiocManager::install(lib)
+# And finally we install the required packages
+for(pkg in install_bioconductor_pkgs) BiocManager::install(pkg)
 
-# Required R packages
-cran_pkgs <- c('dplyr','DT','htmltools','httr','jsonlite','magrittr','methods','parallel','purrr','shiny','shinyjs','tibble','tools','utils')
+# Extract all required CRAN packages
+cran_pkgs <- required_pkgs[which(!required_pkgs %in% bioconductor_pkgs)]
 
-# Select only the packages that aren't currently installed.
-install_cran_lib <- cran_pkgs[!cran_pkgs %in% utils::installed.packages()]
+# Select only the packages that aren't currently installed in the system
+install_cran_pkgs <- cran_pkgs[which(!cran_pkgs %in% utils::installed.packages())]
 
-# And finally we install the missing R packages, including their dependency.
-for(lib in install_cran_lib) utils::install.packages(lib, dependencies = TRUE, repos='http://cran.rstudio.com/')
+# And finally we install the required packages including their dependencies
+for(pkg in install_cran_pkgs) utils::install.packages(pkg, dependencies = TRUE, repos='http://cran.rstudio.com/')
 
 
 
